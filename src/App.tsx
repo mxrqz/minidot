7db1a3a112d6b8da15efc46c43bcf67b2cb756f1
mxrqz <msw940220@gmail.com>
2026-01-14 22:52:44 -0300

    bg-v4

--- src/App.tsx
+++ src/App.tsx
@@ -12,6 +12,7 @@ import SearchModal from "./ui/SearchModal";
 import SettingsModal from "./ui/SettingsModal";
 import Toast from "./ui/Toast";
 import UndoToast from "./ui/UndoToast";
+import GridBackground from "./ui/GridBackground";
 // Initialize theme on app load
 import "./state/themeStore";
 
@@ -107,6 +108,7 @@ function App() {
         boxShadow: "inset 0 1px 0 rgba(255, 255, 255, 0.06), inset 0 -1px 0 rgba(0, 0, 0, 0.04)",
       }}
     >
+      <GridBackground />
       <PageContent onPageChange={handlePageChange} />
       <PageDots />
       <Toast />
